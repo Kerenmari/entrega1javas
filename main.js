8cd6a9e6f2dba880b8fb79bf708a9e837a6e9f50
keren <kerenbarreto9@gmail.com>
2024-08-16 15:44:46 -0400

    comienzo

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log(`EJERCICIO 1`);
+console.log(`=====================`);
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(`el numero ${i} es par`);
+  } else {
+    console.log(`el numero ${i} es impar`);
+  }
+}
