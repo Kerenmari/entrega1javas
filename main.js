4ea6ed18c9ad3063aa41b6cca41ce7bc8ac42e58
keren <kerenbarreto9@gmail.com>
2024-08-18 15:27:07 -0400

    fin-de-semana

--- main.js
+++ main.js
@@ -9,38 +9,44 @@ for (let i = 1; i <= 10; i++) {
   }
 }
 
-console.log(`=====================`);
+
 console.log(`EJERCICIO 2`);
-
-function compara(num1 = 0, num2 = 0) {
-  if (num1 > num2) {
-    return num1 + " es mayor" + num2;
-  } else if (num1 < num2) {
-    return num2 + "es mayor" + num1;
-  } else {
-    return "Ambos números son iguales";
-  }
-}
-console.log(compara(8, 3));
-console.log(compara(2, 2));
-
 console.log(`=====================`);
+
+
 console.log(`EJERCICIO 3`);
-
 console.log(`=====================`);
+
+
 console.log(`EJERCICIO 4`);
+console.log(`=====================`);
 
 for (let i = 0; i <= 12; i++) {
   console.log(i);
 }
 
-console.log(`=====================`);
+
 console.log(`EJERCICIO 5`);
+console.log(`=====================`);
 
 for (let i = 1; i <= 6; i++) {
   console.log(`Mes`);
   console.log(i);
 }
 
-console.log(`=====================`);
+
 console.log(`EJERCICIO 6`);
+console.log(`=====================`);
+
+const meses = ["enero", "febrero", "marzo", "abril", "Mayo"];
+for (let i = 0; i < 5; i++){
+  console.log(meses [i])
+}
+
+
+console.log(`EJERCICIO 7`);
+console.log(`=====================`);
+
+
+console.log(`EJERCICIO 8`);
+console.log(`=====================`);
