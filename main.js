26dc85da07bacd28e517796b0b2fd06d35523fe7
keren <kerenbarreto9@gmail.com>
2024-08-23 17:50:08 -0400

    modificacion

--- main.js
+++ main.js
@@ -13,6 +13,16 @@ for (let i = 1; i <= 10; i++) {
 console.log(`EJERCICIO 2`);
 console.log(`=====================`);
 
+function comparar(num1, num2){
+  if(num1 >  num2){
+      console.log(`el numero ${num1} es mayor ${num2}`)
+  } else if(num1 < num2){
+      console.log(`el numero ${num1} es menor ${num2}`)
+  } else{
+      console.log('el numero es igual')
+  }
+  }
+comparar(67,77)
 
 console.log(`EJERCICIO 3`);
 console.log(`=====================`);
